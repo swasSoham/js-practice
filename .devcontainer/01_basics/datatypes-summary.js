7d7c9c9382ca0b59d02b3afeacaeda18c9205bc2
Swastik Sahu <123658568+swasSoham@users.noreply.github.com>
2024-05-26 11:08:45 +0000

    Datatypes sumnmary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+//Primitives
+
+//7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 200
+
+const bigNumber = 45454545454n
+
+// Reference (Non primitive)
+// Array, Objects, Functions
+
+const heros = ["Aba", "Daba", "Chaaba"];
+
+let myObj = {
+    name: "Swastik",
+    age: 22
+}
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+//Reference (Non-primitive)
+//Arrays, Objects, Functions
+
+//undefined => undefined
+//null => object
+//boolean => boolean
+//number => number
+//string => string
+//object(native and does not implement [Call]) : object
+//object(native or host and does implenment[Call] : function
+//object(host and does not implement[Call]) : Implenmentation defined except may not be "undefined", "boolean", "number" or "string"
+
+
+console.log(typeof bigNumber);
+
+
+
+console.log(typeof anotherId);
