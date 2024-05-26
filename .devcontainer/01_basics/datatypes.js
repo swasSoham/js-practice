8cdbf5494d3c73c223597cedf55be6846b2e41ca
Swastik Sahu <123658568+swasSoham@users.noreply.github.com>
2024-05-26 09:20:56 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,13 @@
+//code readability should be high
+
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+// number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique
