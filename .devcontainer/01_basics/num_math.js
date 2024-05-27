7cfc5f04bf1f52746211053b592149b459a1dceb
Swastik Sahu <123658568+swasSoham@users.noreply.github.com>
2024-05-27 12:47:43 +0000

    math and num

---
+++ .devcontainer/01_basics/num_math.js
@@ -0,0 +1,36 @@
+const score = 100
+//console.log(score);
+
+const balance = new Number(200)
+//console.log(balance);
+
+//console.log(balance.toString());
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(4));
+
+const otherNum = 456.678
+//console.log(otherNum.toPrecision(5));
+
+const hundreds = 10000000000
+//console.log(hundreds.toLocaleString());
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//MATHS
+
+//console.log(Math);
+//console.log(Math.abs(-56));
+//console.log(Math.round(2.4));
+//console.log(Math.floor(4.9));   //will return bottom 
+//console.log(Math.ceil(4.1));   //will return top
+
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
